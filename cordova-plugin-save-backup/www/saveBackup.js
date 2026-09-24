c2f1cdaf4c842e6915c011194bdf6c0fcb3498a3
Minecade <anitabugade42@gmail.com>
2026-09-24 11:44:14 +0530

    Consolidate APK build and native backup export into one workflow

--- cordova-plugin-save-backup/www/saveBackup.js
+++
@@ -1,7 +0,0 @@
-var exec = require('cordova/exec');
-
-module.exports = {
-    create: function (json, fileName, success, error) {
-        exec(success || function () {}, error || function () {}, 'SaveBackup', 'create', [json, fileName]);
-    }
-};
